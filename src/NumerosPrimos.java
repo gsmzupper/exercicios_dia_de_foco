f92ecec22a1145bdae45bd1b5fa6dbbd4714a0f7
gsmzupper <gabriel.merconi@zup.com.br>
2024-12-06 15:42:36 -0300

    Adiciona a classe NumerosPrimos que verifica se o numero digitado pelo usuario é primo

---
+++ src/NumerosPrimos.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\tDigite um número e descubra se ele é primo. Ou digite um número negativo para sair.");
+            int valor = sc.nextInt();
+
+            if (valor < 0) {
+                System.out.println("\tAté a próxima!");
+                break;
+            } else {
+                boolean ePrimo = true;
+                if (valor <= 1) {
+                    ePrimo = false;
+                } else {
+                    for (int i = 2; i < Math.sqrt(valor); i++) {
+                        if (valor % i == 0) {
+                            ePrimo = false;
+                            break;
+                        }
+                    }
+                }
+                if (ePrimo) {
+                    System.out.println("\tO número " + valor + " é primo!");
+                } else {
+                    System.out.println("\tO número " + valor + " não é primo.");
+                }
+            }
+        }
+        sc.close();
+    }
+}
